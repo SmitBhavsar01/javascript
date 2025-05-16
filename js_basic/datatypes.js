987f7ab1f5c80d44836014256a59d001756ca010
Smit.Bhavsar <smitbhavsar1992@gmail.com>
2025-05-16 18:58:50 +0000

    done

---
+++ js_basic/datatypes.js
@@ -0,0 +1,20 @@
+// alert() used in browser not in nodejs
+
+/*
+ number -- range till 2 to power 53
+ bigint --- big numbers
+ string ---in double or single " ' "
+ boolean --- true/false
+ null----standalone value
+ symbol -- for uniquenes find 
+ undefined -- variable declared  but value not given
+*/
+
+
+let name =" smit"
+let age = 20
+let is_logged_In = false
+
+
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
---
+++ js_basic/variable.js
@@ -0,0 +1,15 @@
+const account_id = 144553
+let account_email = "smit@gmail.com"
+var account_password = "12345"
+account_city = "jaipur"
+let account_state;
+
+
+//account_id = 2
+account_email = "smmgeit@gmail.com"
+account_password = "123565645"
+account_city = "banglore"
+
+
+console.log(account_state);
+console.table([account_email,account_id,account_password,account_city])
