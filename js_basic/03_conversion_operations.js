58b4a17d4ce2aad8566be74a2fe8b74f0d1342be
Smit.Bhavsar <smitbhavsar1992@gmail.com>
2025-05-17 13:51:20 +0000

    done

--- js_basic/03_conversion_operations.js
+++ js_basic/03_conversion_operations.js
@@ -1,14 +1,32 @@
-let score = "true"
+/*let score = "true"
  
 console.log(typeof score)
 console.log(typeof(score))
 
 let value_in_number = Number(score) 
 console.log(typeof value_in_number);
-console.log(value_in_number);
+console.log(value_in_number); */
 
 /*
  "33"---33
  "33abc" => NaN
   true => 1; false => 0
+*/
+/*
+let str1 = "hello"
+let str2 = " smit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+ 2+2);
+console.log(1+ 2+"2");
+console.log(3+4*5%3);// dont make code like this use () this for more redaiability
+*/
+
+/*let num1 , num2, num3
+num1 = num2 = num3 = 2+2 
+
+=== check both value and datatype
 */
