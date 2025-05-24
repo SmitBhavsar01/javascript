e2544ebf55d15999c77fd9ff908097868df11bf7
Smit.Bhavsar <smitbhavsar1992@gmail.com>
2025-05-24 17:13:13 +0000

    for_in_loop_done

---
+++ js_iteration/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb:"rubby",
+    swift: "swift by apple"
+}
+
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut for ${myObject[key]}`);
+    
+    }
+
+    const programming = ["js","rb","python","java","cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+    }
+
+    const map = new Map()
+map.set('IN', "INDIA")
+map.set('IN', "INDIA")
+map.set('USA',"United State Of America")
+map.set('FR',"France")
+
+
+for (const key in map) {
+        console.log(key);
+        
+    }
